8eb165afc2ed69131c600da494db8fb255b961b8
Dennie Nguyen <50893065+streamville@users.noreply.github.com>
2024-03-28 17:00:56 -0400

    initial commit

---
+++ src/data/index.js
@@ -0,0 +1,12 @@
+import { faker } from "@faker-js/faker";
+// This file has nothing to do with Redux
+// It exports functions that create random
+// movies and song
+
+export const createRandomMovie = () => {
+  return `${faker.word.adjective()} ${faker.word.noun()}`;
+};
+
+export const createRandomSong = () => {
+  return faker.music.songName();
+};
